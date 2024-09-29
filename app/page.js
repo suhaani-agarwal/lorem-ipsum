39db87d8720ff9621d0e9c878ad75ccc40fddc39
Suhaani Agarwal <suhaani0707@gmail.com>
2024-09-29 11:07:36 +0530

    Your commit message

--- app/page.js
+++ app/page.js
@@ -1,10 +1,15 @@
 import Image from "next/image";
 
-
 export default function Home() {
   return (
-    <div>
-      <img className="w-screen h-screen opacity-35 bg-black" src="https://media.istockphoto.com/id/1088102900/photo/young-indian-women-group-having-fun-together.jpg?s=612x612&w=0&k=20&c=8pGwwyjWvhHe2Jo1w1hnB2tzPNU786m2TVxGonCMVx8=" alt="" />
+    <div >
+      {/* <div className="bg-[url('https://media.istockphoto.com/id/1088102900/photo/young-indian-women-group-having-fun-together.jpg?s=612x612&w=0&k=20&c=8pGwwyjWvhHe2Jo1w1hnB2tzPNU786m2TVxGonCMVx8=')] h-screen w-screen opacity-35">
+      <div className="opacity-100 w-full h-full">Empower Her</div>
+      </div> */}
+      <div></div>
+      <div className="m-5 text-xl font-bold">FillInTheGaps</div>
+      <div className="relative left-20 top-40 text-6xl text-black font-bold w-[60%]">Filling the voids of life just like LoremIpsum!</div>
+      <div className="relative left-20 top-48 bg-blue-400 text-white w-[170px] h-11  text-center font-bold rounded-xl flex items-center justify-center"><button>Get Started</button></div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundImage:{
+        "homebg" : "url('https://media.istockphoto.com/id/1088102900/photo/young-indian-women-group-having-fun-together.jpg?s=612x612&w=0&k=20&c=8pGwwyjWvhHe2Jo1w1hnB2tzPNU786m2TVxGonCMVx8=')"
+      }
     },
   },
   plugins: [],
