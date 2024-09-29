41eca8349c4a8f6523d7dfbba0dcbd983e2d66f0
Suhaani Agarwal <suhaani0707@gmail.com>
2024-09-29 09:47:09 +0530

    second

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      
+      <img className="w-screen h-screen opacity-35 bg-black" src="https://media.istockphoto.com/id/1088102900/photo/young-indian-women-group-having-fun-together.jpg?s=612x612&w=0&k=20&c=8pGwwyjWvhHe2Jo1w1hnB2tzPNU786m2TVxGonCMVx8=" alt="" />
     </div>
   );
 }
